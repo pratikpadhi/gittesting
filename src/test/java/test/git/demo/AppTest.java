f8bb0c26fb699ecc9fbb6b291e07a301be672359
test id <duplicaterajesh@gmail.com>
2020-06-02 06:52:13 +0530

    commit

---
+++ src/test/java/test/git/demo/AppTest.java
@@ -0,0 +1,19 @@
+package test.git.demo;
+
+import org.testng.annotations.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+   @Test
+   public void testcase()
+   {
+	   System.out.println("Hello world");
+   }
+   
+}
